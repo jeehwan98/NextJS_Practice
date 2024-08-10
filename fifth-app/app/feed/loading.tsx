8d4432de9a9dfc52ae6d3bad2ea9011080c68c2a
Jee Hwan <jeehwan98@naver.com>
2024-08-10 20:55:30 +0800

    Mutating Data useActionState

---
+++ fifth-app/app/feed/loading.tsx
@@ -0,0 +1,3 @@
+export default function FeedLoading() {
+  return <p className="loading">Loading feed data...</p>;
+}
---
+++ fifth-app/app/new-post/error.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+interface ErrorProps {
+  error: {
+    message: string
+  }
+}
+
+export default function NewPostError({ error }: ErrorProps) {
+  return (
+    <>
+      <h2>An error occurred!</h2>
+      <p>Unfortunately, something went wrong.</p>
+      <p>{error.message}</p>
+    </>
+  );
+}
